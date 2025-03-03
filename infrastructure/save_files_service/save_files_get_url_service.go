c2e8c33d5bfed33448deefbfe3283e1e8e52573e
Juan David Sierra <judas3991@gmail.com>
2025-03-03 16:53:41 -0500

    no return in get files by data

--- infrastructure/save_files_service/save_files_get_url_service.go
+++ infrastructure/save_files_service/save_files_get_url_service.go
@@ -1,8 +1,6 @@
 package savefilesservice
 
 import (
-	savefileslibs "github.com/jSierraB3991/SimpleApiSaveFiles/domain/save_files_libs"
-	savefilesresponse "github.com/jSierraB3991/SimpleApiSaveFiles/infrastructure/save_files_response"
 	jsierralibs "github.com/jSierraB3991/jsierra-libs"
 )
 
@@ -10,13 +8,8 @@ func (c *SaveFileService) GetImageUrlTemp(imagePaths []string, folder string) ([
 	var result []jsierralibs.ImageNameTemp
 
 	for _, v := range imagePaths {
-		var resultTemp savefilesresponse.PaginationEntry
-		err := c.client.Get(&resultTemp, savefileslibs.SEARCH_ENTRIES+v)
-		if err != nil {
-			return nil, err
-		}
 		result = append(result, jsierralibs.ImageNameTemp{
-			UrlImageTemp: *resultTemp.Data[0].URL,
+			UrlImageTemp: v,
 			ImageName:    v,
 		})
 	}
