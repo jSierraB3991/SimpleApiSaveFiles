8508fbd5d21860ae367e0334b15e82b2195ff121
Juan David Sierra <judas3991@gmail.com>
2025-03-03 17:22:27 -0500

    fix in service interface

--- domain/save_files_service_interface/service_interface.go
+++ domain/save_files_service_interface/service_interface.go
@@ -5,12 +5,13 @@ import (
 	"mime/multipart"
 
 	savefilesresponse "github.com/jSierraB3991/SimpleApiSaveFiles/infrastructure/save_files_response"
+	jsierralibs "github.com/jSierraB3991/jsierra-libs"
 )
 
 type SaveFilesServiceeInterface interface {
 	UploadImage(file *multipart.FileHeader, folder string) (string, error)
 	DeleteImage(folder, path string) error
-	GetImageUrlTemp(imagePaths []string, folder string) ([]interface{}, error)
+	GetImageUrlTemp(imagePaths []string, folder string) ([]jsierralibs.ImageNameTemp, error)
 	GetInfoFolderService(folderName string) (*savefilesresponse.EntryResponse, error)
 	GetImageByUrl(imageUrl string) (*bytes.Buffer, error)
 }
